bd2cf84d914d7ed42eeec73dc2685d5e0af8f72c
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-03-14 22:00:08 +0700

    LC-create-target-array-in-the-given-order [Runtime: 0 ms; Memory: 19.1 MB]

--- array/create-target-array-in-the-given-order.py
+++ array/create-target-array-in-the-given-order.py
@@ -1,6 +1,6 @@
 class Solution:
     def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
-        target = []
-        for i in range(len(nums)):
-            target.insert(index[i], nums[i])
-        return target
+        arr = []  
+        for num, idx in zip(nums, index):
+            arr.insert(idx, num)
+        return arr
