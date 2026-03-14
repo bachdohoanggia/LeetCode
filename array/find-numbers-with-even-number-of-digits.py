955bde9362ce5bc5d95ae4ae672ae9ea22a0f219
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-03-14 22:14:16 +0700

    LC-find-numbers-with-even-number-of-digits [Runtime: 0 ms; Memory: 19.3 MB]

--- array/find-numbers-with-even-number-of-digits.py
+++ array/find-numbers-with-even-number-of-digits.py
@@ -2,5 +2,5 @@ class Solution:
     def findNumbers(self, nums: List[int]) -> int:
         count = 0
         for num in nums: 
-            count += 1 if (len(str(nums)) % 2 == 0) else 0
+            count += 1 if (len(str(num)) % 2 == 0) else 0
         return count
