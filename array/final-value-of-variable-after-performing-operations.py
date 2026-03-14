fe95cb8afcf354c98a5aa04abb33778e881afc72
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-03-14 22:07:58 +0700

    LC-final-value-of-variable-after-performing-operations [Runtime: 2 ms; Memory: 19.2 MB]

--- array/final-value-of-variable-after-performing-operations.py
+++ array/final-value-of-variable-after-performing-operations.py
@@ -1,9 +1,9 @@
 class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
         x = 0
-        for i in range(len(operations)):
-            if operations[i] == "--X" or operations[i] == "X--":
+        for operation in operations:
+            if operation == "--X" or operation == "X--":
                 x -= 1
-            elif operations[i] == "++X" or operations[i] == "X++":
+            else:
                 x += 1
         return x
