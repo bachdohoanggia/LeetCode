4306a4b8d78dbd4d452aa55ec09aa7fc5d37ed86
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-03-19 21:35:28 +0700

    LC-squares-of-a-sorted-array [Runtime: 11 ms; Memory: 21.3 MB]

--- two-pointers/squares-of-a-sorted-array.py
+++ two-pointers/squares-of-a-sorted-array.py
@@ -1,6 +1,26 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)):
-            nums[i] = nums[i] * nums[i]
-        nums.sort()
-        return nums
+        positive = []
+        negative = []
+        ans = []
+        for num in nums:
+            if num < 0:
+                negative.append(num*num)
+            else:
+                positive.append(num*num)
+
+        negative.reverse()
+
+        i = j = 0
+        while i < len(positive) and j < len(negative):
+            if positive[i] <= negative[j]:
+                ans.append(positive[i])
+                i += 1
+            else:
+                ans.append(negative[j])
+                j += 1
+        ans.extend(positive[i:])
+        ans.extend(negative[j:])
+
+        return ans
+        
