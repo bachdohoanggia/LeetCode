54bad8bbd555f633a3c23c27c210a5b623620872
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-03-20 21:35:59 +0700

    LC-check-if-a-string-is-an-acronym-of-words [Runtime: 0 ms; Memory: 19.3 MB]

---
+++ string/check-if-a-string-is-an-acronym-of-words.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAcronym(self, words: List[str], s: str) -> bool:
+        arr = []
+        for word in words:
+            arr.append(word[0])
+        if s == ''.join(arr):
+            return True
+        else:
+            return False
