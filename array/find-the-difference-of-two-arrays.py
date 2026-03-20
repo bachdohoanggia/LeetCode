46d4e12b8595cf9a013a534cf722c5cd05d607f0
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-03-20 21:58:25 +0700

    LC-find-the-difference-of-two-arrays [Runtime: 432 ms; Memory: 19.3 MB]

---
+++ array/find-the-difference-of-two-arrays.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        i = 0
+        diff1 = []
+        diff2 = []
+        for num1 in nums1:
+            if num1 not in nums2 and num1 not in diff1:
+                diff1.append(num1)
+        for num2 in nums2:
+            if num2 not in nums1 and num2 not in diff2:
+                diff2.append(num2)
+        return [diff1, diff2]
