68cbc3c0f7477686528a3e4b8a8cec2ecd106a7d
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-03-21 21:45:32 +0700

    LC-find-the-difference-of-two-arrays [Runtime: 459 ms; Memory: 19.5 MB]

--- hash-table/find-the-difference-of-two-arrays.py
+++ hash-table/find-the-difference-of-two-arrays.py
@@ -1,6 +1,5 @@
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        i = 0
         diff1 = []
         diff2 = []
         for num1 in nums1:
