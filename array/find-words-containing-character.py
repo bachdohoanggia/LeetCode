c42122e14de55bb71fc18baf7b800bc8dd2c009e
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-03-21 21:52:41 +0700

    LC-find-words-containing-character [Runtime: 0 ms; Memory: 19.4 MB]

--- array/find-words-containing-character.py
+++ array/find-words-containing-character.py
@@ -1,7 +1,7 @@
 class Solution:
     def findWordsContaining(self, words: List[str], x: str) -> List[int]:
         arr = []
-        for i in range(len(words)):
-            if x in words[i]:
+        for i, word in enumerate(words):
+            if x in word:
                 arr.append(i)
         return arr
