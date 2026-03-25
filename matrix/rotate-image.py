4779f5510e9b99ba0fa137cd2b7598c1dfe341a9
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-03-25 17:10:39 +0700

    LC-rotate-image [Runtime: 0 ms; Memory: 19.2 MB]

---
+++ matrix/rotate-image.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+        for mat in matrix:
+            mat.reverse()      
+        
+        return matrix
