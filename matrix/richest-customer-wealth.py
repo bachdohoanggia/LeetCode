cbdeceb8f706eed9d7d294f8f327064744b0137b
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-03-27 23:30:49 +0700

    LC-richest-customer-wealth [Runtime: 0 ms; Memory: 19.3 MB]

--- matrix/richest-customer-wealth.py
+++ matrix/richest-customer-wealth.py
@@ -1,3 +1,3 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        return max(sum(account) for account in accounts) # đây k phải O(1) ạ? e khum bt nx
+        return max(sum(account) for account in accounts)
