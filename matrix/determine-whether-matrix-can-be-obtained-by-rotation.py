0581becaead421b4cf3d76b34639027ebea66813
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-03-28 00:19:01 +0700

    LC-determine-whether-matrix-can-be-obtained-by-rotation [Runtime: N/A; Memory: N/A]

--- matrix/determine-whether-matrix-can-be-obtained-by-rotation.py
+++ matrix/determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -2,10 +2,10 @@ class Solution:
     def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
         n = len(mat)
         ans = [[0] * n for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                ans[j][n - i - 1] = mat[i][j]
         for _ in range(4):
-            for i in range(n):
-                for j in range(n):
-                    ans[i][j] = mat[n - i - 1][j]
-                    if ans == target:
-                        return True
+            if ans == target:
+                return True
         return False
