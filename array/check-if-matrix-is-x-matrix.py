668c9418927513422a7bae00f90c114342bb1aec
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-03-27 22:12:56 +0700

    LC-check-if-matrix-is-x-matrix [Runtime: 7 ms; Memory: 20.2 MB]

--- array/check-if-matrix-is-x-matrix.py
+++ array/check-if-matrix-is-x-matrix.py
@@ -1,13 +1,12 @@
 class Solution:
     def checkXMatrix(self, grid: List[List[int]]) -> bool:
         n = len(grid)
-        total = 0
         for i in range(n):
             for j in range(n):
-                if i == j and i == n - j - 1:
+                if i == j or i == n - j - 1:
                     if grid[i][j] == 0:
                         return False
-                elif i != j and i != n - j - 1:
+                else:
                     if grid[i][j] != 0:
                         return False
         return True
