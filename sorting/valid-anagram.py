a9e5f7c56c34a7a5317eeb6169af750079e6941c
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-02 20:06:32 +0700

    LC-valid-anagram [Runtime: N/A; Memory: N/A]

--- sorting/valid-anagram.py
+++ sorting/valid-anagram.py
@@ -4,10 +4,10 @@ class Solution:
         for char in s:
             if char not in s_freq:
                 s_freq[char] = 0
-            s_freq[char] = 1
+            s_freq[char] = +1
         t_freq = {}
         for char in t:
             if char not in t_freq:
                 t_freq[char] = 0
-            t_freq[char] = 1
+            t_freq[char] = +1
         return s_freq == t_freq
