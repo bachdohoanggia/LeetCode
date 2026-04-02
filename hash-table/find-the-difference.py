bf6318ff714d9fe15c2c460a9c37feb16789df57
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-02 22:25:32 +0700

    LC-find-the-difference [Runtime: 3 ms; Memory: 19.1 MB]

--- hash-table/find-the-difference.py
+++ hash-table/find-the-difference.py
@@ -1,5 +1,9 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
+        freq = {}
+        for char in s:
+            freq[char] = freq.get(char, 0) + 1
         for char in t:
-            if char not in s:
+            if char not in freq or freq[char] == 0:
                 return char
+            freq[char] -= 1
