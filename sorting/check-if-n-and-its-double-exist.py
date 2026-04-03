b14f4bbb2bb8d450b4745366b98c103b8541f695
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-03 23:16:09 +0700

    LC-check-if-n-and-its-double-exist [Runtime: 3 ms; Memory: 19.1 MB]

--- sorting/check-if-n-and-its-double-exist.py
+++ sorting/check-if-n-and-its-double-exist.py
@@ -1,8 +1,11 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
         n = len(arr)
+        ans = []
         for i in range(n):
-                if arr[i]/2 in arr or arr[i]*2 in arr:
-                    return True
+            if arr[i]/2 not in ans and arr[i]*2 not in ans:
+                ans.append(arr[i])
+            else:
+                return True
         return False
                 
