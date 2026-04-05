d195b7ed1dea97b6d464261513807dcf9a1e232a
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-05 22:09:12 +0700

    LC-majority-element [Runtime: 7 ms; Memory: 21.2 MB]

--- sorting/majority-element.py
+++ sorting/majority-element.py
@@ -1,9 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        freq = {}
-        n = len(nums)
-        for num in nums:
-            freq[num] = freq.get(num, 0) + 1
-        for num, f in freq.items():
-            if f >= n/2:
-                return num
+        freq = Counter(nums)
+        return freq.most_common()[0][0]
