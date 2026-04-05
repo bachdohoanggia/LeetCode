82d7afea844edf160feee8c31070082f320bbc5e
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-05 22:02:24 +0700

    LC-valid-anagram [Runtime: 6 ms; Memory: 19.3 MB]

--- sorting/valid-anagram.py
+++ sorting/valid-anagram.py
@@ -1,13 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_freq = {}
-        for char in s:
-            if char not in s_freq:
-                s_freq[char] = 0
-            s_freq[char] += 1
-        t_freq = {}
-        for char in t:
-            if char not in t_freq:
-                t_freq[char] = 0
-            t_freq[char] += 1
-        return s_freq == t_freq
+        return Counter(s) == Counter(t)
