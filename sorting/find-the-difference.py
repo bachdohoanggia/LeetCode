4bb9a792f714716d2591bd66db608ae30bf41182
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-05 22:05:45 +0700

    LC-find-the-difference [Runtime: 5 ms; Memory: 19.5 MB]

--- sorting/find-the-difference.py
+++ sorting/find-the-difference.py
@@ -1,9 +1,4 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        freq = {}
-        for char in s:
-            freq[char] = freq.get(char, 0) + 1
-        for char in t:
-            if char not in freq or freq[char] == 0:
-                return char
-            freq[char] -= 1
+        for k in (Counter(t) - Counter(s)).keys():
+            return k
