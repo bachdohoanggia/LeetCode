0c2382f83ac43264555852dca929818c18d85c98
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-05 22:54:33 +0700

    LC-group-anagrams [Runtime: 12 ms; Memory: 22 MB]

---
+++ sorting/group-anagrams.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = {}
+        for s in strs:
+            sorted_s = "".join(sorted(list(s)))
+            if sorted_s not in groups:
+                groups[sorted_s] = []
+            
+            groups[sorted_s].append(s)
+        return list(groups.values())
+            
+            
+        
+        
