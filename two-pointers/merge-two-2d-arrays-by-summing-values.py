58bc235042c2c06ce1a6fa2204de3e6607e1702b
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-11 16:11:07 +0700

    LC-merge-two-2d-arrays-by-summing-values [Runtime: 3 ms; Memory: 19.4 MB]

---
+++ two-pointers/merge-two-2d-arrays-by-summing-values.py
@@ -0,0 +1,24 @@
+class Solution:
+    def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        arr = []
+        i, j = 0, 0
+        m, n = len(nums1), len(nums2)
+        while i < m and j < n:
+            if nums1[i][0] < nums2[j][0]:
+                arr.append(nums1[i])
+                i += 1
+            elif nums1[i][0] > nums2[j][0]:
+                arr.append(nums2[j])
+                j += 1
+            else:
+                arr.append([nums1[i][0], nums1[i][1] + nums2[j][1]])
+                i += 1
+                j += 1
+        while i < m:
+            arr.append(nums1[i])
+            i += 1
+        while j < n:
+            arr.append(nums2[j])
+            j += 1
+
+        return arr
