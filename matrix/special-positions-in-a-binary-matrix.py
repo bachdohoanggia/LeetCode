262f1c9dbcb0c1e34d10f9662ceb2be70f998e6f
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-11 14:26:16 +0700

    LC-special-positions-in-a-binary-matrix [Runtime: N/A; Memory: N/A]

--- matrix/special-positions-in-a-binary-matrix.py
+++ matrix/special-positions-in-a-binary-matrix.py
@@ -3,13 +3,13 @@ class Solution:
         m, n = len(mat), len(mat[0])
         row = [0] * n
         col = [0] * m
-        for i in range(m):
-            for j in range(n):
+        for i in range(n):
+            for j in range(m):
                 row[i] += mat[i][j]
                 col[j] += mat[i][j]
         count = 0
-        for i in range(m):
-            for j in range(n):
+        for i in range(n):
+            for j in range(m):
                 if row[i] == 1 and col[j] == 1 and mat[i][j] == 1:
                     count += 1
         return count
