c2d5603c5a1c565d719e26606f308fb6b3ea73a5
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-11 14:20:28 +0700

    LC-special-positions-in-a-binary-matrix [Runtime: N/A; Memory: N/A]

--- matrix/special-positions-in-a-binary-matrix.py
+++ matrix/special-positions-in-a-binary-matrix.py
@@ -1,15 +1,15 @@
 class Solution:
     def numSpecial(self, mat: List[List[int]]) -> int:
         m, n = len(mat), len(mat[0])
-        row = [0] * n
-        col = [0] * m
-        for i in range(n):
+        row = [0] * m
+        col = [0] * n
+        for i in range(m):
             for j in range(m):
                 row[i] += mat[i][j]
                 col[j] += mat[i][j]
         count = 0
-        for i in range(n):
-            for j in range(m):
+        for i in range(m):
+            for j in range(n):
                 if row[i] == 1 and col[j] == 1 and mat[i][j] == 1:
                     count += 1
         return count
