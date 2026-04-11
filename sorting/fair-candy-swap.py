29ef35e9cfbdfb617ca6c65c7bc57e11c22faa3d
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-11 14:57:10 +0700

    LC-fair-candy-swap [Runtime: N/A; Memory: N/A]

--- sorting/fair-candy-swap.py
+++ sorting/fair-candy-swap.py
@@ -1,10 +1,7 @@
 class Solution:
     def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
-        sumA = sum(aliceSizes)
-        sumB = sum(bobSizes)
-        diff = (sumA - sumB) / 2
         bob_set = set(bobSizes)
         for x in aliceSizes:
-            y = x - diff
+            y = x - (sumA - sumB) / 2
             if y in bob_set:
                 return [x, y]
