b08086dbe22e16757ca8b6cb1ae3c79006ab5ced
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-17 13:08:47 +0700

    LC-keyboard-row [Runtime: 0 ms; Memory: 19.2 MB]

--- string/keyboard-row.py
+++ string/keyboard-row.py
@@ -1,16 +1,15 @@
 class Solution:
     def findWords(self, words: List[str]) -> List[str]:
-        set1 = set('qwertyuiop')
-        set2 = set('asdfghjkl')
-        set3 = set('zxcvbnm')
+        set1 = set("qwertyuiop")
+        set2 = set("asdfghjkl")
+        set3 = set("zxcvbnm")
         sets = [set1, set2, set3]
         result = []
         for s in sets:
-            for word in words :
+            for word in words:
                 for ch in word.lower():
                     if ch not in s:
                         break
                 else:
                     result.append(word)
         return result
-                        
