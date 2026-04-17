d846d0bd4d0b6bda4c06330dc9bb1249e5379b88
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-17 23:04:44 +0700

    LC-find-the-town-judge [Runtime: 11 ms; Memory: 22.7 MB]

--- hash-table/find-the-town-judge.py
+++ hash-table/find-the-town-judge.py
@@ -1,11 +1,11 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        trust_in = [0] * (n + 1)
-        trust_out = [0] * (n + 1)
+        tr_in = [0] * (n+1)
+        tr_out = [0] * (n+1)
         for a, b in trust:
-            trust_out[a] += 1
-            trust_in[b] += 1
+            tr_out[a] += 1
+            tr_in[b] += 1
         for i in range(1, n + 1):
-            if trust_in[i] == n - 1 and trust_out[i] == 0:
+            if tr_out[i] == 0 and tr_in[i] == n-1:
                 return i
         return -1
