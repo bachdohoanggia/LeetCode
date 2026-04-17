14d3d9efcc7f7a3cd35df623c92fde7bab374136
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-17 18:26:53 +0700

    LC-find-words-that-can-be-formed-by-characters [Runtime: 187 ms; Memory: 19.7 MB]

--- array/find-words-that-can-be-formed-by-characters.py
+++ array/find-words-that-can-be-formed-by-characters.py
@@ -1,7 +1,8 @@
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
+        char_ct = Counter(chars)
         result = 0 
         for word in words:
-            if Counter(word) - Counter(chars) == Counter():
+            if Counter(word) - char_ct == Counter():
                 result += len(word)
         return result
