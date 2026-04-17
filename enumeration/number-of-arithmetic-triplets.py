4e7c3efe514c78f670c955b47a9840cddcea3e57
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-17 22:42:55 +0700

    LC-number-of-arithmetic-triplets [Runtime: 0 ms; Memory: 19.2 MB]

--- enumeration/number-of-arithmetic-triplets.py
+++ enumeration/number-of-arithmetic-triplets.py
@@ -3,6 +3,6 @@ class Solution:
         num_set = set(nums)
         count = 0
         for num in nums:
-            if num + diff in num_set and num + 2 * diff in num_set:
+            if num + diff in num_set and num - diff in num_set:
                 count += 1
         return count
