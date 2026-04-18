5fee053b8acf10c0643fa01d1763b0bc906b98a2
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-18 23:13:06 +0700

    LC-maximum-score-after-splitting-a-string [Runtime: 2 ms; Memory: 19.2 MB]

--- prefix-sum/maximum-score-after-splitting-a-string.py
+++ prefix-sum/maximum-score-after-splitting-a-string.py
@@ -1,13 +1,13 @@
 class Solution:
     def maxScore(self, s: str) -> int:
         n = len(s)
-        pref0 = [0] * n
-        suff1 = [0] * n
+        pref0 = defaultdict(int)
+        suff1 = defaultdict(int)
         
         for i, ch in enumerate(s):
-            pref0[i] = int(ch == '0') + (pref0[i - 1] if i > 0 else 0)
+            pref0[i] = int(ch == '0') + pref0[i - 1]
         for i in range(n - 1, -1, -1):
-            suff1[i] = int(s[i] == '1') + (suff1[i + 1] if i < n - 1 else 0)
+            suff1[i] = int(s[i] == '1') + suff1[i + 1]
         best = 0
         for i in range(n - 1):
             best = max(best, pref0[i] + suff1[i + 1])
