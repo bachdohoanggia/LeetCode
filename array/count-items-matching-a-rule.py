c5c80fe9234293c6e227042b5eb29183400dc8c6
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-20 18:58:39 +0700

    LC-count-items-matching-a-rule [Runtime: 0 ms; Memory: 23.6 MB]

--- array/count-items-matching-a-rule.py
+++ array/count-items-matching-a-rule.py
@@ -1,14 +1,16 @@
 class Solution:
     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        RULE_KEY_MAP = {
+            "type": 0,
+            "color": 1,
+            "name": 2
+        }
+        
+        index = RULE_KEY_MAP[ruleKey]
         count = 0
+        
         for item in items:
-            if ruleKey == "type":
-                if item[0] == ruleValue:
-                    count += 1
-            if ruleKey == "color":
-                if item[1] == ruleValue:
-                    count += 1
-            if ruleKey == "name":
-                if item[2] == ruleValue:
-                    count += 1
+            if item[index] == ruleValue:
+                count += 1
+                
         return count
