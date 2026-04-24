dd770b7dbddc10629187eb3b82dd3472308a922b
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-24 14:12:39 +0700

    LC-maximum-score-after-splitting-a-string [Runtime: N/A; Memory: N/A]

--- prefix-sum/maximum-score-after-splitting-a-string.py
+++ prefix-sum/maximum-score-after-splitting-a-string.py
@@ -7,11 +7,13 @@ class Solution:
         for i , ch in enumerate(s):
             pref0[i] = (int(ch == "0")) + pref0[i - 1]
             
+            
 
         for i in range(n - 1, -1, -1):
             suff1[i] = (int(s[i] == "1")) + suff1[i + 1]
+            
 
         best = 0
-        for i in range(n - 1):
+        for i in range(n):
             best = max(best, pref0[i] + suff1[i + 1])
         return best
