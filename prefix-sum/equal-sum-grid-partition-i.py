c3fcc4f48679699cd0202fd69085103254216a6a
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-26 19:41:02 +0700

    LC-equal-sum-grid-partition-i [Runtime: 133 ms; Memory: 41.6 MB]

---
+++ prefix-sum/equal-sum-grid-partition-i.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canPartitionGrid(self, grid: List[List[int]]) -> bool:
+        m, n = len(grid), len(grid[0])
+        total = sum(sum(row) for row in grid)
+        row_sum, col_sum = 0, 0
+
+        if total % 2 != 0:
+            return False
+        for i in range(m - 1):
+            row_sum += sum(grid[i])
+            if row_sum == total / 2:
+                return True
+
+        for j in range(n - 1):
+            col_sum += sum(row[j] for row in grid)
+            if col_sum == total / 2:
+                return True
+
+        return False
+
+        
+                 
