75937f2419f93b43de0e501e894f56f15146fd3e
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-04-27 19:33:59 +0700

    LC-continuous-subarray-sum [Runtime: 54 ms; Memory: 39.4 MB]

--- prefix-sum/continuous-subarray-sum.py
+++ prefix-sum/continuous-subarray-sum.py
@@ -1,13 +1,13 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         remaining_map = {0: -1}
-        prefix_sum = 0
+        prefix = 0
         for i, num in enumerate(nums):
-            prefix_sum += num
-            remaining = prefix_sum % k
+            prefix += num
+            remaining = prefix % k
 
             if remaining in remaining_map:
-                if i - remaining_map[remaining] >= 2:
+                if i - remaining_map[remaining] >=2:
                     return True
             else:
                 remaining_map[remaining] = i
