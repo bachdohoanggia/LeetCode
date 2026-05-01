b55f2f7c28a4417956b4ddcdd2a6a6a1820f7aa2
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-05-01 17:16:42 +0700

    LC-corporate-flight-bookings [Runtime: 31 ms; Memory: 30.9 MB]

---
+++ array/corporate-flight-bookings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        
+        arr = [0] * (n + 2)
+        for book in bookings:
+            arr[book[0]] += book[2]
+            arr[book[1] + 1] -= book[2]
+        
+        answer = [0] * (n + 1)
+        answer[0] = arr[0]
+        for i in range(1, n + 1):
+            answer[i] = answer[i - 1] + arr[i]
+        return answer[1:]
+
