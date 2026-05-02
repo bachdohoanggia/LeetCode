9fd19b4f66008a533a18cf59aa6449216a08a47e
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-05-02 22:22:48 +0700

    LC-find-all-anagrams-in-a-string [Runtime: 142 ms; Memory: 19.9 MB]

--- string/find-all-anagrams-in-a-string.py
+++ string/find-all-anagrams-in-a-string.py
@@ -2,11 +2,12 @@ class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         lp = len(p)
         ls = len(s)
+        target = Counter(p)
         window = Counter(s[:len(p)])
-        res = [0] if (window == Counter(p)) else []
+        res = [0] if (window == target) else []
         for right in range(lp, ls):
             window[s[right]] += 1
             window[s[right - lp]] -= 1
-            if window == Counter(p):
+            if window == target:
                 res.append(right - lp + 1)
         return res
