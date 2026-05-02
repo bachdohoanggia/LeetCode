ee1c3a0dc0ac2504dc2dba7f8801cd539a37432e
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-05-02 21:06:46 +0700

    LC-number-of-ways-to-split-array [Runtime: 56 ms; Memory: 33.6 MB]

--- prefix-sum/number-of-ways-to-split-array.py
+++ prefix-sum/number-of-ways-to-split-array.py
@@ -1,13 +1,9 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
-        n = len(nums)
-        prefix = [0] * n
-        prefix[0] = nums[0]
+        prefix = list(accumulate(nums))
         count = 0
-        total = sum(nums)
-        for i in range(1, n):
-            prefix[i] = prefix[i - 1] + nums[i]
-        for i in range(n - 1):
+        total = prefix[-1]
+        for i in range(len(nums) - 1):
             if prefix[i] >= total - prefix[i]:
                 count += 1
         return count
