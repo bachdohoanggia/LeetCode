51e52cf20cd543d2f0f7001c9961d29634e37fc7
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-05-07 00:19:21 +0700

    LC-maximum-average-subarray-i [Runtime: 63 ms; Memory: 29.7 MB]

--- array/maximum-average-subarray-i.py
+++ array/maximum-average-subarray-i.py
@@ -5,6 +5,6 @@ class Solution:
         max_sum = pref[k - 1]
         for i in range(k, n):
             current = pref[i] - pref[i - k]
-            max_sum = max(current / k, max_sum)
-        return max_sum
+            max_sum = max(current, max_sum)
+        return max_sum / k 
         
