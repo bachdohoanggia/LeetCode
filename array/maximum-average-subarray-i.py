8dff3d61ed305d85b131528853da0988e56b2eca
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-05-07 00:05:41 +0700

    LC-maximum-average-subarray-i [Runtime: 69 ms; Memory: 29 MB]

--- array/maximum-average-subarray-i.py
+++ array/maximum-average-subarray-i.py
@@ -2,8 +2,9 @@ class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         n = len(nums)
         window = sum(nums[:k])
-        best_avg =  window/k
+        best_avg = window / k
         for right in range(k, n):
             window = window + nums[right] - nums[right - k]
-            best_avg = max(best_avg, window/k)
+            best_avg = max(best_avg, window / k)
         return best_avg
+        
