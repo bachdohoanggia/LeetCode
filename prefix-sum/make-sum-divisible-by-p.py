e3a76f083252a0cf5156664910e4f777494fc91a
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-05-07 22:12:32 +0700

    LC-make-sum-divisible-by-p [Runtime: N/A; Memory: N/A]

---
+++ prefix-sum/make-sum-divisible-by-p.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minSubarray(self, nums: List[int], p: int) -> int:
+        total = sum(nums) % p 
+        if total == 0: 
+            return 0
+        prefix = {0: -1}
+        cur = 0
+        best_len = inf
+
+        for i, num in enumerate(nums):
+            cur = (cur + num) % p
+            target = (cur - total) % p
+
+            if target in prefix:
+                best_len = min(best_len, i - prefix[target])
+
+            prefix[cur] = i
+        return best_len
+
+            
+            
