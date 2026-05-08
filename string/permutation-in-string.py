cebc958e0d032d2f04adae26aeb3da67df231373
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-05-08 19:01:17 +0700

    LC-permutation-in-string [Runtime: N/A; Memory: N/A]

---
+++ string/permutation-in-string.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        count1 = defaultdict(int)
+        count2 = defaultdict(int)
+
+        n1, n2 = len(s1), len(s2)
+
+        for i in range(n1):
+            count1[s1[i]] += 1
+
+        for i in range(n1):
+            count2[s2[i]] += 1
+
+        if count1 == count2:
+            return True
+        
+        for right in range(n1, n2):
+            count2[s2[right]] += 1
+            count2[s2[right- n1]] -= 1
+            if count2[s2[right- n1]] == 0:
+                del count2[s2[right- n1]]
+            if count1 == count2:
+                return True
+        return False
+        
+        
