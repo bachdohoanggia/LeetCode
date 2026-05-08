653ea556fe32192fad9a4e3e861bc6efacb5fd3e
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-05-08 18:39:46 +0700

    LC-maximum-sum-of-almost-unique-subarray [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/maximum-sum-of-almost-unique-subarray.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxSum(self, nums: List[int], m: int, k: int) -> int:
+        n = len(nums)
+        if n < k:
+            return 0
+
+        count = defaultdict(int)
+        window = 0
+
+        for i in range(k):
+            count[nums[i]] += 1
+            window += nums[i]
+        max_sum = window if len(count) >= m else 0
+        
+        for right in range(k, n):
+            window = window + nums[right] - nums[right - k]
+            if count[nums[right - k]] == 0:
+                del count[nums[right - k]]
+            count[nums[right]] += 1
+            if len(count) >= m:
+                max_sum = max(max_sum, window)
+        return max_sum
+
+            
