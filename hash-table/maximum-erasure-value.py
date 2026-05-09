f71a9028b5e86e6a5dcc4df7369c54fdaa21f6bd
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-05-09 14:42:02 +0700

    LC-maximum-erasure-value [Runtime: N/A; Memory: N/A]

--- hash-table/maximum-erasure-value.py
+++ hash-table/maximum-erasure-value.py
@@ -2,11 +2,14 @@ class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
         ans = 0
         left = 0
+        total = 0
         window = set()
         for digit in nums:
             while digit in window:
                 window.remove(nums[left])
+                total -= nums[left]
                 left += 1
             window.add(digit)
-            ans = max(ans, sum(window))
+            total += digit
+            ans = max(ans, total)
         return ans
