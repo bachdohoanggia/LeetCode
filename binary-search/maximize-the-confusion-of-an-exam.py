8a67ac63cfd9db5f732dc983b1a15126a77fed4c
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-05-09 14:18:36 +0700

    LC-maximize-the-confusion-of-an-exam [Runtime: N/A; Memory: N/A]

---
+++ binary-search/maximize-the-confusion-of-an-exam.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        n = len(answerKey)
+        left = 0
+        count = defaultdict(int)
+        max_freq, max_len = 0, 0
+        for right in range(n):
+            count[answerKey[right]] += 1
+            max_freq = max(max_freq, count[answerKey[right]])
+            while (right - left + 1) - max_freq > k:
+                count[answerKey[left]] -= 1
+                left += 1
+            max_len = max(max_len, right - left + 1)
+        return max_len
+
