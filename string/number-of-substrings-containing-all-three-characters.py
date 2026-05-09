0500fccd220c0fde1652f40c13a68333fd6783fb
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-05-09 23:16:33 +0700

    LC-number-of-substrings-containing-all-three-characters [Runtime: N/A; Memory: N/A]

---
+++ string/number-of-substrings-containing-all-three-characters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        n = len(s)
+        ans = 0
+        left = 0
+        count = defaultdict(int)
+
+        for right in range(n):
+            count[s[right]] += 1
+
+            while len(count) == 3:
+                ans += n - right
+                count[s[left]] -= 1
+                if count[s[left]] == 0:
+                    del count[s[left]]
+                left += 1
+        return ans
