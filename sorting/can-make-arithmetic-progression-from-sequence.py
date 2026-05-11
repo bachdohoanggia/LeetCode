b40ca3a89ed1ea426c4cc93df738374ec8d0afcc
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-05-11 22:15:52 +0700

    LC-can-make-arithmetic-progression-from-sequence [Runtime: N/A; Memory: N/A]

---
+++ sorting/can-make-arithmetic-progression-from-sequence.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        n = len(arr)
+        arr.sort()
+        for i in range(n - 2):
+            if abs(arr[i] - arr[i + 1]) == abs(arr[i + 1] - arr[i + 2]):
+                return True
+        return False
