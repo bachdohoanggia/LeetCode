76bf04cde600dd00e4ef4edf91c3ef2ddd2c4cea
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-05-11 22:48:05 +0700

    LC-palindrome-number [Runtime: N/A; Memory: N/A]

--- math/palindrome-number.py
+++ math/palindrome-number.py
@@ -1,9 +1,13 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
         str_x = str(x)
-        if str_x == str_x[::-1]:
+        n = len(str_x)
+        palindrome = ''
+        for i in range(n - 1, -1, -1):
+            palindrome += str_x[i]
+        if int(palindrome) == x:
             return True
-
         return False
 
-        
