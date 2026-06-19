5101ca7010b64c93f125ab437a5c4da6dd62aa15
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-06-19 17:06:26 +0700

    LC-how-many-numbers-are-smaller-than-the-current-number [Runtime: N/A; Memory: N/A]

--- sorting/how-many-numbers-are-smaller-than-the-current-number.py
+++ sorting/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        sortnums = sorted(nums)
+        sorted_num = sorted(nums)
         for i in range(len(nums)):
-            nums[i] = sortnums.index(nums[i])
+            nums[i] = sorted_num.index(nums[i])
         return nums
