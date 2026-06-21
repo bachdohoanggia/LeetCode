409d509226976438d5091ed253af2b54a508e324
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-06-21 19:40:12 +0700

    LC-matrix-diagonal-sum [Runtime: N/A; Memory: N/A]

--- matrix/matrix-diagonal-sum.py
+++ matrix/matrix-diagonal-sum.py
@@ -1,9 +1,10 @@
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
-        ans = 0
         n = len(mat)
+        c = n // 2
+        total = 0
         for i in range(n):
-            ans += mat[i][i] + mat[i][n - i - 1]
+            total += mat[i][i] + mat[i][n - i - 1]
         if n % 2 != 0:
-            ans -= mat[int(n/2)][int(n/2)]
-        return ans
+            return total - mat[c][c]
+        return total
