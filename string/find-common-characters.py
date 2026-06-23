8521dfd54cccdf26864c64a1f4cca0d91d1bdd3d
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-06-23 18:25:27 +0700

    LC-find-common-characters [Runtime: N/A; Memory: N/A]

--- string/find-common-characters.py
+++ string/find-common-characters.py
@@ -1,19 +1,12 @@
-from typing import List
-
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        # Khởi tạo với từ đầu tiên
         freq = Counter(words[0])
-        
-        # Với mỗi từ tiếp theo
         for word in words[1:]:
 
-            # Đếm tần suất của từ hiện tại
             word_freq = Counter(word)
             
-            # Cập nhật freq = min(freq, word_freq)
-            for char, f in freq.items():
-                freq[char] = min(f, word_freq.get(char, 0))
+            for ch, f in freq.items():
+                freq[ch] = min(f, word_freq.get(ch,0))
             
         return list(freq.elements())
         
