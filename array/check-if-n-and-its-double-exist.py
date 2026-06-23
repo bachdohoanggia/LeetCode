1435e38f0ff2061ba696b5da179e5e9e40fb18c2
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-06-23 17:03:11 +0700

    LC-check-if-n-and-its-double-exist [Runtime: N/A; Memory: N/A]

--- array/check-if-n-and-its-double-exist.py
+++ array/check-if-n-and-its-double-exist.py
@@ -2,9 +2,8 @@ class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
         seen = set()
         for num in arr:
-            if num/2 not in seen and num*2 not in seen:
-                seen.add(num)
-            else:
+            if num / 2 in seen or num * 2 in seen:
                 return True
+            else:
+                seen.add(num)
         return False
-                
