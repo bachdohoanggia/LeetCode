16f1859f59cfde838e2632a0a8d867b3f28c4820
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-06-24 22:47:20 +0700

    LC-count-largest-group [Runtime: N/A; Memory: N/A]

--- math/count-largest-group.py
+++ math/count-largest-group.py
@@ -4,9 +4,8 @@ class Solution:
         for num in range(1, n+1):
             digit_sum = sum(int(d) for d in str(num))
             groups[digit_sum] += 1
-        
-        max_size = max(groups.values())
 
+        max_size = max(groups.values())
         count = 0
         for group in groups.values():
             if group == max_size:
