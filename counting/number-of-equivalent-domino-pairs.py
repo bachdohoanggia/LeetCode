61654a1a5a8c2e84912718b64d3a7b8c137c8d46
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-06-24 23:05:16 +0700

    LC-number-of-equivalent-domino-pairs [Runtime: N/A; Memory: N/A]

--- counting/number-of-equivalent-domino-pairs.py
+++ counting/number-of-equivalent-domino-pairs.py
@@ -1,11 +1,9 @@
 class Solution:
     def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
         freq = Counter()
-        pairs = 0
+        pair = 0
         for a, b in dominoes:
-            keys = (min(a, b), max(a, b))
-            pairs += freq[keys]
+            keys = (min(a, b), max(a,b))
+            pair += freq[keys]
             freq[keys] += 1
-            
-        return pairs
-
+        return pair
