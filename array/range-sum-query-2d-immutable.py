33676da9701081d07f005eb7f3fa8fb11c40e454
bachdohoanggia <dohoanggiabach3003@gmail.com>
2026-06-30 20:00:29 +0700

    LC-range-sum-query-2d-immutable [Runtime: N/A; Memory: N/A]

--- array/range-sum-query-2d-immutable.py
+++ array/range-sum-query-2d-immutable.py
@@ -2,7 +2,7 @@ class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
         m, n = len(matrix), len(matrix[0])
-        self.prefix = [[0] * (n) for _ in range(m)]
+        self.prefix = [[0] * n for _ in range(m)]
         for i in range(m):
             for j in range(n):
                 rec1 = self.prefix[i-1][j] if i > 0 else 0
@@ -11,9 +11,9 @@ class NumMatrix:
                 self.prefix[i][j] = rec1 + rec2 - rec3 + matrix[i][j]
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        rec1 = self.prefix[row1-1][col2] if row1 > 0 else 0
-        rec2 = self.prefix[row2][col1-1] if col1 > 0 else 0
-        rec3 = self.prefix[row1-1][col1-1] if (row1 > 0 and col1 > 0) else 0
+        rec1 = self.prefix[row1 - 1][col2] if row1 > 0 else 0
+        rec2 = self.prefix[row2][col1 - 1] if col2 > 0 else 0
+        rec3 = self.prefix[row1 - 1][col1 - 1] if (row1 > 0 and col1 > 0) else 0
         return self.prefix[row2][col2] - rec1 - rec2 + rec3
 
 
